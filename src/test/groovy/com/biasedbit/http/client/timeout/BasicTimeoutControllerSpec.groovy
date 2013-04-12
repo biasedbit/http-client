31b2569c39c357a9d8d57ad2f5f80dd3567f4b8a
Bruno de Carvalho <bruno@biasedbit.com>
2013-04-12 04:44:52 +0100

    Test basic timeout controller's multiple constructors

--- src/test/groovy/com/biasedbit/http/client/timeout/BasicTimeoutControllerSpec.groovy
+++ src/test/groovy/com/biasedbit/http/client/timeout/BasicTimeoutControllerSpec.groovy
@@ -10,6 +10,8 @@ import org.jboss.netty.handler.codec.http.HttpVersion
 import spock.lang.Specification
 import spock.lang.Unroll
 
+import java.util.concurrent.Executors
+
 /**
  * @author <a href="http://biasedbit.com/">Bruno de Carvalho</a>
  */
@@ -24,9 +26,21 @@ class BasicTimeoutControllerSpec extends Specification {
 
   def cleanup() { controller.terminate() }
 
+  def "it accepts N > 0 as constructor argument and creates a fixed size thread pool"() {
+    expect: new BasicTimeoutController(1)
+  }
+
+  def "it doesn't terminate an external executor when its created with one"() {
+    given: def executor = Executors.newCachedThreadPool()
+    and: def controller = new BasicTimeoutController(executor)
+
+    when: controller.terminate()
+    then: !executor.terminated
+  }
+
   @Unroll
   def "it times out a request when the time out is #timeout ms and #sleepTime ms have elapsed"() {
-    setup: def context = createContext(timeout)
+    given: def context = createContext(timeout)
     and: controller.controlTimeout(context);
     when: sleep(sleepTime)
     then: context.getFuture().isDone()
