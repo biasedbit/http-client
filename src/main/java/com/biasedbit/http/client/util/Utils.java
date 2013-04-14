26d9c02daca38d41bfb8319062be2b129e8fd24f
Bruno de Carvalho <bruno@biasedbit.com>
2013-04-14 04:21:13 +0100

    Delete methods that don't support formattable description

--- src/main/java/com/biasedbit/http/client/util/Utils.java
+++ src/main/java/com/biasedbit/http/client/util/Utils.java
@@ -9,19 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Utils {
 
-    public static void ensureValue(boolean condition, String description)
-            throws IllegalArgumentException {
-        if (!condition) throw new IllegalArgumentException(description);
-    }
-
     public static void ensureValue(boolean condition, String description, Object... args)
             throws IllegalArgumentException {
         if (!condition) throw new IllegalArgumentException(String.format(description, args));
-    }
-
-    public static void ensureState(boolean condition, String description)
-        throws IllegalStateException {
-        if (!condition) throw new IllegalStateException(description);
     }
 
     public static void ensureState(boolean condition, String description, Object... args)
